376d813e52ad056d3e3953858a3aaf0c3d055ccf
Douglas Hellberg <DougHellberg@gmail.com>
2023-07-12 16:34:54 -0400

    new lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "Greg"
+function upperCaseCustomerName(){
+   
+    customerName = customerName.toUpperCase();
+    
+}
+function setBestCustomer(){
+    window.bestCustomer ='not bob';
+    
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Chris"
+}
